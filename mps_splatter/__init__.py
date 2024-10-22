18d3655494819592a801881d4d4f4246d4973a83
Thibaut CHAUFFIER <tchauffi@gmail.com>
2024-10-22 22:19:43 +0200

    ADD: init file

---
+++ mps_splatter/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is the main module for the mps_splatter package.
+"""
+# Package metadata
+__version__ = "0.1.0"
+__author__ = "Thibaut CHAUFFIER"
+__email__ = "tchauffi@gmail.com"
+__description__ = "This package provides an implementation of a 3D gaussian splatting running on MPS."
